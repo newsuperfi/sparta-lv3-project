1a78e37e2bec9ce9906ead2453c428af4c90374d
Wonhee Rhyu <rhyu0207@gmail.com>
2023-06-22 23:39:58 +0900

    .

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Comments, Posts } = require('../models');
+const { Comments } = require('../models');
 const auth = require('../middlewares/auth');
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Users, Posts, Comments } = require('../models');
+const { Users } = require('../models');
 const { Op } = require('sequelize');
 const jwt = require('jsonwebtoken');
 
